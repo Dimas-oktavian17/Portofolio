b9e4450ce62bed72af0100753d55e680fea40ba4
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2022-09-09 21:24:19 +0700

    change project section

--- public/src/index.js
+++ public/src/index.js
@@ -74,16 +74,16 @@ var swiper = new Swiper(".mySwiper", {
 
   breakpoints: {
     640: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     768: {
       slidesPerView: 1,
-      spaceBetween: 10,
+      spaceBetween: 20,
     },
     1024: {
       slidesPerView: 3,
-      spaceBetween: 10,
+      spaceBetween: 20,
     },
   },
 });
