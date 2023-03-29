b7b3309f3f09ae599e185feab317644f142f4a14
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-03-29 14:15:39 +0700

    fix content

--- public/src/index.js
+++ public/src/index.js
@@ -1,36 +1,36 @@
 // Testimoni Dom
-let starOne = document.querySelector(".starSatu");
-let starTwo = document.querySelector(".starDua");
-let starThree = document.querySelector(".starTiga");
-let starFour = document.querySelector(".starEmpat");
-let deskripsiTestimoni = document.querySelector(
-  '[aria-label="text-testimoni"]'
-);
-let isiTestimoni =
-  "“Aplikasi sialan, ini gw jadi kagak bisa alesan lagi buat bolos kelas Pak Sanusi yang galak itu! Tapi aku sekarang jadi rajin kuliah, terima kasih garena!”";
-document.querySelector(".btn").addEventListener("click", () => {
-  deskripsiTestimoni.textContent = isiTestimoni;
-  starOne.classList.remove("bg-[#FFC93F]");
-  starTwo.classList.remove("bg-[#FFC93F]");
-  starThree.classList.remove("bg-[#FFC93F]");
-  starFour.classList.remove("bg-[#FFC93F]");
-  starOne.classList.add("bg-gray-300");
-  starTwo.classList.add("bg-gray-300");
-  starThree.classList.add("bg-gray-300");
-  starFour.classList.add("bg-gray-300");
-});
-document.querySelector(".btn-2").addEventListener("click", () => {
-  deskripsiTestimoni.textContent =
-    "“Sebenernya ngga jago-jago amat sih cuma bisa basic-basic, masalah skill tinggal belajar nanti.”";
-  starOne.classList.remove("bg-gray-300");
-  starTwo.classList.remove("bg-gray-300");
-  starThree.classList.remove("bg-gray-300");
-  starFour.classList.remove("bg-gray-300");
-  starOne.classList.add("bg-[#FFC93F]");
-  starTwo.classList.add("bg-[#FFC93F]");
-  starThree.classList.add("bg-[#FFC93F]");
-  starFour.classList.add("bg-[#FFC93F]");
-});
+// let starOne = document.querySelector(".starSatu");
+// let starTwo = document.querySelector(".starDua");
+// let starThree = document.querySelector(".starTiga");
+// let starFour = document.querySelector(".starEmpat");
+// let deskripsiTestimoni = document.querySelector(
+//   '[aria-label="text-testimoni"]'
+// );
+// let isiTestimoni =
+//   "“Aplikasi sialan, ini gw jadi kagak bisa alesan lagi buat bolos kelas Pak Sanusi yang galak itu! Tapi aku sekarang jadi rajin kuliah, terima kasih garena!”";
+// document.querySelector(".btn").addEventListener("click", () => {
+//   deskripsiTestimoni.textContent = isiTestimoni;
+//   starOne.classList.remove("bg-[#FFC93F]");
+//   starTwo.classList.remove("bg-[#FFC93F]");
+//   starThree.classList.remove("bg-[#FFC93F]");
+//   starFour.classList.remove("bg-[#FFC93F]");
+//   starOne.classList.add("bg-gray-300");
+//   starTwo.classList.add("bg-gray-300");
+//   starThree.classList.add("bg-gray-300");
+//   starFour.classList.add("bg-gray-300");
+// });
+// document.querySelector(".btn-2").addEventListener("click", () => {
+//   deskripsiTestimoni.textContent =
+//     "“Sebenernya ngga jago-jago amat sih cuma bisa basic-basic, masalah skill tinggal belajar nanti.”";
+//   starOne.classList.remove("bg-gray-300");
+//   starTwo.classList.remove("bg-gray-300");
+//   starThree.classList.remove("bg-gray-300");
+//   starFour.classList.remove("bg-gray-300");
+//   starOne.classList.add("bg-[#FFC93F]");
+//   starTwo.classList.add("bg-[#FFC93F]");
+//   starThree.classList.add("bg-[#FFC93F]");
+//   starFour.classList.add("bg-[#FFC93F]");
+// });
 
 // Set Time out Compleate project
 let total = document.getElementById("total");
@@ -43,14 +43,14 @@ let interval = setInterval(() => {
 //
 setTimeout(() => {
   clearInterval(interval);
-}, 22000);
+}, 8000);
 
-// }, 1000);
+// // }, 1000);
 
-// About btn
-document.getElementById("closeBtn").addEventListener("click", () => {
-  document.getElementById("card").classList.toggle("blur-sm");
-});
+// // About btn
+// document.getElementById("closeBtn").addEventListener("click", () => {
+//   document.getElementById("card").classList.toggle("blur-sm");
+// });
 // Shadow navbar
 let nav = document.querySelector(".fixed");
 window.addEventListener("scroll", () => {
